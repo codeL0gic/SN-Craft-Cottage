e793b58821e9dd58d43d4b59fc6724ab391493c9
codeL0gic <shannon.neumann@yum.com>
2025-03-26 18:11:04 -0400

    added ReadMe and WMap comment out

--- wmap.js
+++ wmap.js
@@ -1,6 +1,5 @@
 "use strict"(
-  //weather map API Development
-
+  /*weather map API Development
   function () {
     const url =
       "api.openweathermap.or/data/2.5/weather?lat={lat}&lon={lon}&appid={d86c4488004cf927b42b4e40b3793ebc}";
@@ -14,5 +13,5 @@ document
   .addEventListener("click", function (e) {
     e.preventDefault();
     getWeatherData();
-  });
+  });--*/
 //get weather data when user clicks Forecast button, then add temp & conditions to view//
